6a733b1aa75ec1e2269399945167caf1d44ecb18
testerai10x <test.ai.10x@gmail.com>
2023-10-17 22:56:00 -0600

    cypress update and StandardLogin testcase

--- cypress/e2e/integration/Test_Integration.cy.js
+++ cypress/e2e/integration/Test_Integration.cy.js
@@ -1,7 +1,22 @@
 /// <reference types="cypress" />
 
 describe('Login', () => {
-  it('should login', () => {
-    cy.visit('https://bstackdemo.com/')
+
+  beforeEach(function(){
+
+    cy.fixture('LoginData').then((data) => {
+      this.data = data
+    })
+
+    cy.visit('https://bstackdemo.com/signin')
+
+  })
+
+
+  it('Standard Login', function(){
+    
+    cy.Login(this.data.username.demo, this.data.password.pass)
+    cy.get(this.data.message.demo_success).should('be.visible')
+
   })
 })
--- cypress/support/login-commands.js
+++ cypress/support/login-commands.js
@@ -1,2 +1,9 @@
 /// <reference types="cypress" />
 
+Cypress.Commands.add('Login', (email, password) => {
+    cy.fixture('LoginElements').then((loginElements) => {
+        cy.get(loginElements.username).type(email + '{enter}')
+        cy.get(loginElements.password).type(password + '{enter}')
+        cy.get(loginElements.loginButton).click()
+    })
+})
